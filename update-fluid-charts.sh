7968c99623e362e13cf6682934574a585002681e
cheyang <cheyang@163.com>
2023-03-12 15:56:14 +0800

    Update to alpha.4, To #46827170
    Signed-off-by: cheyang <cheyang@163.com>

--- update-fluid-charts.sh
+++ update-fluid-charts.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-#!/bin/bash
-
 set -e # Set to stop execution on error
 
+charts_dir="charts"
+fluid_dir="$charts_dir/fluid"
+fluid_source_dir="../fluid/$charts_dir/fluid"
+
 # Ensure the current directory is the 'charts' directory
-if [ "$(basename $(pwd))" != "charts" ]; then
+if [ "$(basename $(pwd))" != "$charts_dir" ]; then
   echo "Error: The script should be executed in the 'charts' directory."
   exit 1
 fi
 
 # Ensure the current directory contains the 'charts' subdirectory
-if [ ! -d "charts" ]; then
+if [ ! -d "$charts_dir" ]; then
   echo "Error: The 'charts' directory does not exist in the current directory."
   exit 1
 fi
 
 # Check if the 'fluid' directory exists under the 'charts' directory
-if [ -d "charts/fluid" ]; then
+if [ -d "$fluid_dir" ]; then
   # Remove the 'fluid' directory under the 'charts' directory
-  rm -rf charts/fluid
+  rm -rf "$fluid_dir"
 fi
 
 # Copy the 'fluid' directory in the current directory to the 'charts' directory
-cp ../fluid/charts/fluid/fluid charts
+cp "$fluid_source_dir/fluid" "$charts_dir"
